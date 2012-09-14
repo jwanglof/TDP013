22f953edf265aaa5c3f2743a33c90528733eefb3
Johan Wanglof <jwanglof@gmail.com>
2012-09-14 14:06:11 +0200

    The first 4 points on the speccs are done, more or less

--- Lab2/lib/db.js
+++ Lab2/lib/db.js
@@ -3,41 +3,90 @@ var mongo = require("mongodb");
 var server = new mongo.Server("localhost", 27017);
 var db = new mongo.Db("tdp013lab2", server);
 
-var insertTweet = function(input_message, callback) {
-	db.open(function(err, db) {
-		if (!err) {
-			console.log("DB connected");
-			db.collection("tweets", function(err, collection) {
-				var tweet_msg = {message: input_message, read: 0};
-
-				collection.insert(tweet_msg, function(err, result) {
-					if (!err) {
-						console.log("Inserted: " + input_message + " to the DB");
-					}
-					else {
-						console.log("Couldn't insert: " + input_message + " to the DB");
-					}
-				});
-			});
-		}
-	});
+function output_logger(output_text) {
+	var logger = true;
+	if (logger) {
+		console.log(output_text);
+	}
 }
 
-var getDatabaseCollection = function(dbCollection, callback) {
-	db.open(function(err, db) {
-		if (!err) {
-			console.log("DB connected");
+db.open(function(err, db) {
+	if (!err) {
+		output_logger("DB connected");
 
+		var insertTweet = function(input_message, callback) {
 			db.collection("tweets", function(err, collection) {
+				collection.insert(
+					{
+						message: input_message,
+						read: 0
+					},
+					function(err, result) {
+						if (!err) {
+							output_logger("Inserted: " + input_message + " to the DB");
+							callback(true);
+						}
+						else {
+							output_logger("Couldn't insert: " + input_message + " to the DB");
+							callback(false);
+						}
+					});
+			});
+		}
+
+		var getCollection = function(dbCollection, callback) {
+			db.collection(dbCollection, function(err, collection) {
 				if (!err) {
-					console.log(collection.find({}));
+					collection.find(
+						{
+						}
+					).toArray(
+						function(err, docs) {
+							callback(docs);
+						});
+				}
+				else {
+					output_logger("Couldn't get the collection.");
 				}
 			});
 		}
-	});
-}
 
-exports.insertTweet = insertTweet;
-exports.getDatabaseCollection = getDatabaseCollection;
+		var updateCollection = function(dbCollection, id, JSONvalue, callback) {
+			output_logger("Called updateCollection");
 
+			db.collection(dbCollection, function(err, collection) {
+				if (!err) {
+					var dbId = new mongo.BSONPure.ObjectID.createFromHexString(id);
 
+					collection.update(
+						{
+							_id: dbId
+						},
+						{
+							$set: JSONvalue
+						},
+						{
+							safe: true, //Run callback only when an update is done, false as default
+							upsert: false, //Inserts a new record if not found
+							multi: false //Update all records found, false as default
+						},
+						function(err, count) {
+							if (!err)
+								callback(true);
+							else
+								callback(false);
+						});
+				}
+			});
+		}
+
+		exports.insertTweet = insertTweet;
+		exports.getCollection = getCollection;
+		exports.updateCollection = updateCollection;
+	}
+	else {
+		output_logger("An error occured when connecting to the DB");
+	}
+});
+
+exports.output_logger = output_logger;
--- Lab2/lib/requestHandlers.js
+++ Lab2/lib/requestHandlers.js
@@ -3,15 +3,8 @@ var http = require("http");
 var url = require("url");
 var db = require("./db");
 
-function output_logger(output_text) {
-	var logger = true;
-	if (logger) {
-		console.log(output_text);
-	}
-}
-
 function start(response, postData) {
-	output_logger("Request handler 'start' was called.");
+	db.output_logger("Request handler 'start' was called.");
 
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("Hello Start");
@@ -19,37 +12,67 @@ function start(response, postData) {
 }
 
 function save(response, querys) {
-	output_logger("Request handler 'save' was called.");
+	db.output_logger("Request handler 'save' was called.");
 
 	response.writeHead(200, {"Content-Type": "text/html"});
-
-	db.insertTweet(querys["text"]);
-	response.write("Your text: " + querys["text"] + " was saved to the database. Perhaps anyway, no checks are done. Wopido");
-
-	/*if (message) {
-		response.write("Your text: " + querys["text"] + " was saved to the database.");
+	
+	// Check to see the string isn't empty
+	if (querys["text"]) {
+		db.insertTweet(querys["text"], function(callbackValue) {
+			console.log(callbackValue);
+			if (callbackValue)
+				response.write("Your text: " + querys["text"] + " was saved to the database.");
+			else
+				response.write("The text wasn't saved. Try again");
+		});
 	}
 	else {
-		response.write("Your text couldn't be saved to the database. Try again");
-	}*/
+		response.write("You have to send a text to be inserted into the DB.");
+	}
 
 	response.end();
 }
 
-function flag(response, getData) {
-	output_logger("Request handler 'flag' was called.");
+function flag(response, querys) {
+	db.output_logger("Request handler 'flag' was called.");
 
 	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("Hello Flag");
+	
+	// Check the string so it is the correct length
+	if (querys["id"]) {
+		if (Buffer.byteLength(querys["id"]) == 24) {
+			db.updateCollection("tweets", querys["id"], {read: 2}, function(callbackValue) {
+				// Doesn't work by some unreasonable reason. callbackValue is set to true or false but response won't write anything on the site...
+				console.log(callbackValue);
+
+				if (callbackValue)
+					response.write("The id " + querys["id"] + " is updated.");
+				else
+					response.write("The id " + querys["id"] + " was NOT updated.");
+			});
+		}
+		else if (Buffer.byteLength(querys["id"]) < 24 || Buffer.byteLength(querys["id"]) > 24)
+			response.write("Invalid ID. It has to be 24 characters");
+	}
+	else {
+		response.write("There is no ID to check for. The URL need to have an id-value. <br />");
+		response.write("I.e. flag?id=xxxxxxxxxxxxxxxxxxxxxxxx");
+	}
+	
 	response.end();
 }
 
 function getall(response, getData) {
-	output_logger("Request handler 'getall' was called.");
-
-	db.getDatabaseCollection("tweet_msg");
+	db.output_logger("Request handler 'getall' was called.");
 
 	response.writeHead(200, {"Content-Type": "text/html"});
+
+	// Get all the data from tweets-collection
+	db.getCollection("tweets", function(docs) {
+		response.write(JSON.stringify(docs));
+		db.output_logger(JSON.stringify(docs));
+	});
+
 	response.write("Hello Getall");
 	response.end();
 }
