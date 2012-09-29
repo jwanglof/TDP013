101211a4757f6b923a895a6af3676c65172a3c88
jwanglof <jwanglof@gmail.com>
2012-09-29 09:56:34 +0200

    Removed folders with old files

--- Social_Website/lib/00_OLD/errors/fivehundred.js
+++
@@ -1,7 +0,0 @@
-function error500(response) {
-	response.writeHead(500, {"Content-Type": "text/html"});
-	response.write("500 Internal Server Error <br />");
-	response.end();
-}
-
-exports.error500 = error500;
