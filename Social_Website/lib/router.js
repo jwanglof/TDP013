a70b999e5e439184105063f8e36ad2191da2ab8f
Johan Wanglof <jwanglof@gmail.com>
2012-10-16 12:49:50 +0200

    Added comments

--- Social_Website/lib/router.js
+++ Social_Website/lib/router.js
@@ -1,13 +1,22 @@
+/*
+ * Output logger is a help tool that makes the outputs in the console better, and makes it easier to shut off the logging
+ * Takes 2 arguments: What text to be shown, and which file the text originates from
+ */
 var ol = require("./helpers/output_logger");
+var filename = "router.js";
 
+/*
+ * Handle all routes
+ * If a route isn't specificed in index.js it will return a 404 error
+ */
 function route(handle, response, request, pathname, querys, postData) {
 	if (typeof handle[pathname] === "function") {
-		ol.logger("About to route a request for " + pathname, "router.js");
+		ol.logger("About to route a request for " + pathname, filename);
 
 		handle[pathname](response, request, postData);
 	}
 	else {
-		ol.logger("No request handler found for " + pathname, "router.js");
+		ol.logger("No request handler found for " + pathname, filename);
 
 		response.writeHead(404, {"Content-Type": "text/plain"});
 		response.write("404 Not Found");
--- Social_Website/lib/server.js
+++ Social_Website/lib/server.js
@@ -2,8 +2,19 @@ var http = require("http");
 var url = require("url");
 var querystring = require("querystring");
 
+/*
+ * Output logger is a help tool that makes the outputs in the console better, and makes it easier to shut off the logging
+ * Takes 2 arguments: What text to be shown, and which file the text originates from
+ */
 var ol = require("./helpers/output_logger");
+var filename = "server.js";
 
+var serverPort = 8888;
+
+/*
+ * Handle when there is POST-data available
+ * Add the chunks to postData so it will come as one piece
+ */
 function getPost(req, callback) {
 	var postData = "";
 
@@ -17,31 +28,30 @@ function getPost(req, callback) {
 	});
 }
 
+/*
+ * Create the request server
+ */
 function startServer(route, handle) {
 	function onRequest(request, response) {
-			var pathname = url.parse(request.url).pathname;
+		ol.logger("Request for " + pathname + " received.", filename);
 
-			// The favicon request output isn't shown in the console
-			if (pathname != "/favicon.ico") {
-				ol.logger("Request for " + pathname + " received.", "server.js");
+		var pathname = url.parse(request.url).pathname;
+		var querys = url.parse(request.url, true).query;
 
-				var querys = url.parse(request.url, true).query;
+		request.setEncoding("utf-8");
 
-				request.setEncoding("utf-8");
-
-				// handle contains the handles from index.js
-				// pathname contains ONLY the pathname! E.g. /save
-				// response is the entire output from the server with headers, configuration etc etc
-				// querys contains ONLY the querys! E.g. ?text=someniceandawesometext (in a map)
-				getPost(request, function(result) {
-					route(handle, response, request, pathname, querys, result);
-				});
-			}
+		// handle contains the handles from index.js
+		// pathname contains ONLY the pathname! E.g. /save
+		// response is the entire output from the server with headers, configuration etc etc
+		// querys contains ONLY the querys! E.g. ?text=someniceandawesometext (in a map)
+		getPost(request, function(result) {
+			route(handle, response, request, pathname, querys, result);
+		});
 	}
 
-	http.createServer(onRequest).listen(8888);
+	http.createServer(onRequest).listen(serverPort);
 
-	ol.logger("Server has started.", "server.js");
+	ol.logger("Server has started on port: " + serverPort, filename);
 }
 
 exports.startServer = startServer;
