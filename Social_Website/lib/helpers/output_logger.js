763b1e5669638a5928210692b9275ef3dd8d1e0a
Johan Wanglof <jwanglof@gmail.com>
2012-10-16 12:58:34 +0200

    Made the output prettier. Its sooooo pretty

--- Social_Website/lib/helpers/output_logger.js
+++ Social_Website/lib/helpers/output_logger.js
@@ -1,8 +1,16 @@
-// Exists so it's easy to turn on or off all the console logging
+/*
+ * Easy to turn on or off the logging
+ * Get a pretty output to the console
+ * The for-loop makes sure that the output_text is in line
+ */
 function output_logger(output_text, from_handler) {
 	var logger = true;
 	if (logger) {
-		console.log("(" + from_handler + ") -- " + output_text);
+		var spaces = "";
+		for (var i = from_handler.length; i < 40; i++)
+			spaces += " ";
+
+		console.log("[" + from_handler + "] " + spaces + " " + output_text);
 	}
 }
 
